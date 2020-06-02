76e8751699e3d5345afd9c0b3a8c05522467ea2b
sumitbisht92 <sumitbisht92.com>
2020-06-03 02:21:42 +0530

    Initial commit

---
+++ app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+  import { Router} from '@angular/router';
+  import { Injectable } from '@angular/core';
+  
+  
+  
+  @Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    
+  })
+  export class AppComponent {
+  constructor(public router:Router) {}
+ 
+    
+      
+
+  }
